7276fb7c30718f54ee86b4b5961e7a07bb543574
Nguyễn Tiến Long <email ng k git>
2021-09-18 14:21:39 +0700

    bt

--- src/Java_B3/FindTheLargestValueInAnArray.java
+++ src/Java_B3/FindTheLargestValueInAnArray.java
@@ -10,7 +10,7 @@ public class FindTheLargestValueInAnArray {
         do {
             System.out.println("Enter a size: ");
             size = scanner.nextInt();
-            if(size > 20);
+            if(size > 20)
             System.out.println("Size should not exceed 20");
         } while (size>20);
         array = new int[size];
