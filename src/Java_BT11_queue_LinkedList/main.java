5b7bd49a120c4fddc357909c6db387f41683de4d
Nguyễn Tiến Long <email ng k git>
2021-09-30 14:25:27 +0700

    bt

---
+++ src/Java_BT11_queue_LinkedList/main.java
@@ -0,0 +1,16 @@
+package Java_BT11_queue_LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeued item is " + queue.dequeue().key);
+    }
+}
