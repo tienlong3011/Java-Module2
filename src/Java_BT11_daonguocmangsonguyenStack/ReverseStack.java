72e80e78d49140c404c5a38fe6c7f7b266909927
Nguyễn Tiến Long <email ng k git>
2021-09-30 16:12:01 +0700

    bt

--- src/Java_BT11_daonguocmangsonguyenStack/ReverseStack.java
+++ src/Java_BT11_daonguocmangsonguyenStack/ReverseStack.java
@@ -1,19 +1,42 @@
 package Java_BT11_daonguocmangsonguyenStack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class ReverseStack {
     public static void main(String[] args) {
-        Stack <Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
+        Stack<Integer> stack = new Stack<>();
+        int [] array = {1,2,3,4,5};
+        for (int i = 0; i < array.length; i++) {
+            stack.push(array[i]);
+        }
         System.out.println(stack);
-        int num;
-            num = stack.peek();
-            stack.push(num);
-        System.out.println(stack);
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = stack.peek();
+            stack.pop();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
+        Stack<String> stack2 = new Stack<>();
+        String string = "CodeGyms.com";
+                String [] arr = string.split("");
+                System.out.println("Kết quả: " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            stack2.push(arr[i]);
+        }
+        System.out.println(stack2);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = stack2.peek();
+            stack2.pop();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
